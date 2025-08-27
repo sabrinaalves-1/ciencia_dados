3ffc082bc704c2556aa84539c7ecf980fc9b3859
sabrinaalves-1 <oliveira.alves.sabrina@escola.pr.gov.br>
2025-08-27 11:11:41 +0000

    v3

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -21,5 +21,8 @@ async function visualizarInformacoesGlobais() {
     paragrafo.innerHTML = `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média  <span>${tempoDesTrabalho}horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
 
     const container=document.getElementById('graficos-container')
+       container.appendChild(paragrafo);
 
 }
+
+visualizarInformacoesGlobais()
