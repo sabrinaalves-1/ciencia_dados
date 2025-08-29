c7c67323804a74c6895799be2b12c9d945dd15a6
sabrinaalves-1 <oliveira.alves.sabrina@escola.pr.gov.br>
2025-08-29 13:48:51 +0000

    v5

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,17 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDosPostos = Object.keys(dados)
+    const quantidadeTrabalhadores = Object.values(dados)
+    
+
+const data = [
+    {
+        x: nomeDosPostos,
+        y: quantidadeTrabalhadores,
+        type: 'bar'
+    }
+]
+
+}
