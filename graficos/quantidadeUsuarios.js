eab4ede02bb5667e10f49247fe9d5703b2c01ecf
sabrinaalves-1 <oliveira.alves.sabrina@escola.pr.gov.br>
2025-09-01 14:05:12 +0000

    v11

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -16,7 +16,7 @@ const data = [
 
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
-document.getElementById('graficos-conteiner').appendChild(grafico)
+document.getElementById('graficos-container').appendChild(grafico)
 Plotly.newPlot(grafico, data)
 
 }
